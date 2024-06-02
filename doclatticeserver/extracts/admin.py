093762b451737aabd61d30adc4fcb179c8db94b1
angelo-lacson <angelo.lacson.work0407@gmail.com>
2024-06-02 15:19:06 -0700

    Test extract mutations working again and added tests for the new add and remove contract mutations.

--- doclatticeserver/extracts/admin.py
+++ doclatticeserver/extracts/admin.py
@@ -12,7 +12,7 @@ from doclatticeserver.extracts.models import (
 
 @admin.register(Fieldset)
 class FieldsetAdmin(GuardedModelAdmin):
-    list_display = ["id", "name", "description", "owner"]
+    list_display = ["id", "name", "description"]
 
 
 @admin.register(Column)
