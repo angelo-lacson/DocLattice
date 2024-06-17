e3897f6323474869533f51ff8eb9529eea020cb2
angelo-lacson <angelo.lacson.work0407@gmail.com>
2024-06-16 19:15:01 -0700

    Disconnect document processing signal in test_corpus_query.py

--- doclatticeserver/tests/test_corpus_query.py
+++ doclatticeserver/tests/test_corpus_query.py
@@ -10,6 +10,7 @@ from doclatticeserver.annotations.signals import process_annot_on_create_atomic
 from doclatticeserver.corpuses.models import Corpus, CorpusQuery
 from doclatticeserver.corpuses.signals import run_query_on_create
 from doclatticeserver.documents.models import Document
+from doclatticeserver.documents.signals import process_doc_on_create_atomic
 from doclatticeserver.tasks.doc_tasks import nlm_ingest_pdf
 from doclatticeserver.tasks.embeddings_task import (
     calculate_embedding_for_annotation_text,
@@ -26,6 +27,7 @@ class QueryTasksTestCase(TestCase):
 
         post_save.disconnect(run_query_on_create, sender=CorpusQuery)
         post_save.disconnect(process_annot_on_create_atomic, sender=Annotation)
+        post_save.disconnect(process_doc_on_create_atomic, sender=Document)
 
         self.user = User.objects.create_user(
             username="testuser", password="testpassword"
