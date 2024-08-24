67c8e13e5454f694befd4a2452ee5f051762b120
angelo-lacson <angelo.lacson.work0407@gmail.com>
2024-08-24 12:10:54 -0700

    Added sample contract classifier using gpt4 plus some cleanup in how we kickoff and handle analyses.

--- doclatticeserver/tasks/extract_orchestrator_tasks.py
+++ doclatticeserver/tasks/extract_orchestrator_tasks.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from django.db import transaction
 from django.utils import timezone
 
-from config import celery_app
+from doclatticeserver.utils.celery_tasks import get_task_by_name
 from doclatticeserver.documents.models import DocumentAnalysisRow
 from doclatticeserver.extracts.models import Datacell, Extract
 from doclatticeserver.types.enums import PermissionTypes
@@ -17,16 +17,6 @@ logger = logging.getLogger(__name__)
 
 # Pass OpenAI API key to marvin for parsing / extract
 marvin.settings.openai.api_key = settings.OPENAI_API_KEY
-
-
-def get_task_by_name(task_name) -> Optional[Callable]:
-    """
-    Try to get celery task function Callable by name
-    """
-    try:
-        return celery_app.tasks.get(task_name)
-    except Exception:
-        return None
 
 
 @shared_task
