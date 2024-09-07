babb264b8413f5494b6e9a09e59a1baa849024bc
angelo-lacson <angelo.lacson.work0407@gmail.com>
2024-09-07 08:28:19 -0700

    Update production.py

--- config/settings/production.py
+++ config/settings/production.py
@@ -9,6 +9,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS", default=["doclattice.opensource.legal"]
 )
+CSRF_TRUSTED_ORIGINS=env.list("DJANGO_CSRF_TRUSTED_ORIGINS", default=["https://*.opensource.legal"]
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
