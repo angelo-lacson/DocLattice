c1beb078fe6e7a0600f26f1313a65afcc70ea253
angelo-lacson <angelo.lacson.work0407@gmail.com>
2024-09-11 21:38:12 -0700

    Fixed last (?) test.

--- doclatticeserver/tests/test_permissioning.py
+++ doclatticeserver/tests/test_permissioning.py
@@ -12,22 +12,26 @@ from graphene.test import Client
 from graphql_relay import to_global_id
 
 from config.graphql.schema import schema
+from doclatticeserver.analyzer.models import Analysis, Analyzer, GremlinEngine
+from doclatticeserver.analyzer.signals import install_gremlin_on_creation
+from doclatticeserver.annotations.models import (
+    TOKEN_LABEL,
+    Annotation,
+    AnnotationLabel,
+)
 from doclatticeserver.corpuses.models import Corpus
 from doclatticeserver.documents.models import Document
 from doclatticeserver.tasks.permissioning_tasks import (
     make_analysis_public_task,
     make_corpus_public_task,
 )
-
-from ..analyzer.models import Analysis, Analyzer, GremlinEngine
-from ..analyzer.signals import install_gremlin_on_creation
-from ..annotations.models import TOKEN_LABEL, Annotation, AnnotationLabel
-from ..types.enums import PermissionTypes
-from ..utils.permissioning import (
+from doclatticeserver.types.enums import PermissionTypes
+from doclatticeserver.utils.permissioning import (
     get_users_permissions_for_obj,
     set_permissions_for_obj_to_user,
     user_has_permission_for_obj,
 )
+
 from .fixtures import SAMPLE_PDF_FILE_ONE_PATH
 
 User = get_user_model()
@@ -418,7 +422,8 @@ class PermissioningTestCase(TestCase):
         )
         self.assertEqual(True, make_public_task_results["ok"])
         self.assertEqual(
-            "SUCCESS - Corpus is Public", make_public_task_results["message"]
+            "SUCCESS - Corpus and related objects are now public",
+            make_public_task_results["message"],
         )
 
     def __test_make_analysis_public_mutation(self):
