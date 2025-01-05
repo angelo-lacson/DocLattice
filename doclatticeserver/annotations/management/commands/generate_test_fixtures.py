59c9051a613badedcfe446c35055273c3bf19563
angelo-lacson <angelo.lacson.work0407@gmail.com>
2025-01-05 15:55:27 -0600

    Finally got fixtures to load properly in tests. Adding in relationships as they are needed for higher coverage score.

--- doclatticeserver/annotations/management/commands/generate_test_fixtures.py
+++ doclatticeserver/annotations/management/commands/generate_test_fixtures.py
@@ -235,6 +235,7 @@ class Command(BaseCommand):
                 "documents.document",
                 "annotations.annotationlabel",
                 "annotations.annotation",
+                "annotations.relationship",
             ]
 
             fixture_path = Path("doclatticeserver/tests/fixtures/test_data.json")
