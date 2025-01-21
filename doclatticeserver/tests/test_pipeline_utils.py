9e32e28ba4bf03678545c623954f7b1c93d65602
angelo-lacson <angelo.lacson.work0407@gmail.com>
2025-01-20 18:21:29 -0600

    Ever upwards on the logging...

--- doclatticeserver/tests/test_pipeline_utils.py
+++ doclatticeserver/tests/test_pipeline_utils.py
@@ -103,6 +103,9 @@ from doclatticeserver.pipeline.base.post_processor import BasePostProcessor
 from doclatticeserver.types.dicts import DocLatticeExportDataJsonPythonType
 from doclatticeserver.pipeline.base.file_types import FileTypeEnum
 from typing import List, Tuple
+import logging
+
+logger = logging.getLogger(__name__)
 
 class TestPostProcessor(BasePostProcessor):
     """
@@ -120,9 +123,15 @@ class TestPostProcessor(BasePostProcessor):
         zip_bytes: bytes,
         export_data: DocLatticeExportDataJsonPythonType,
     ) -> Tuple[bytes, DocLatticeExportDataJsonPythonType]:
+        # Add logging to debug the process
+        logger.info("TestPostProcessor.process_export called")
+        logger.info(f"Input export_data: {export_data}")
+        
         # Add a test field to export data
         new_export_data = export_data.copy()
         new_export_data["test_field"] = "test_value"
+        
+        logger.info(f"Modified export_data: {new_export_data}")
         return zip_bytes, new_export_data
 '''
 
