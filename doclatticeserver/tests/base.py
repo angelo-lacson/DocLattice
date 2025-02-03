77d8fb10277406977e6ab9ba2a46b2e701269d39
angelo-lacson <angelo.lacson.work0407@gmail.com>
2025-02-03 01:42:50 -0600

    Test for corpus conversation (and corpus agent factory) working baseline.

--- doclatticeserver/tests/base.py
+++ doclatticeserver/tests/base.py
@@ -15,6 +15,7 @@ from graphql_jwt.shortcuts import get_token
 from config.asgi import application
 from doclatticeserver.annotations.models import Annotation
 from doclatticeserver.annotations.signals import process_annot_on_create_atomic
+from doclatticeserver.corpuses.models import Corpus
 from doclatticeserver.documents.models import Document
 from doclatticeserver.documents.signals import process_doc_on_create_atomic
 
@@ -189,6 +190,8 @@ class BaseFixtureTestCase(TransactionTestCase):
                         self.copy_fixture_file(file_path, media_path)
                         setattr(doc, field, media_path)
             doc.save()
+
+        self.corpus = Corpus.objects.create(title="Test Corpus", creator=self.user, backend_lock=False)
 
 
 class WebsocketFixtureBaseTestCase(BaseFixtureTestCase):
