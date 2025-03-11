c7308ccda9b63f8efe7ba80ca3800b79ad30cfe7
angelo-lacson <angelo.lacson.work0407@gmail.com>
2025-03-11 01:22:58 -0500

    Added minn bert

--- config/settings/base.py
+++ config/settings/base.py
@@ -642,3 +642,11 @@ ANALYZER_KWARGS = {
         "ANTHROPIC_API_KEY": env.str("ANTHROPIC_API_KEY", default=""),
     },
 }
+
+# Minnesota Case Law ModernBERT embedder settings
+MINN_MODERNBERT_EMBEDDERS = {
+    "application/pdf": "doclatticeserver.pipeline.embedders.minn_modern_bert_embedder.MinnModernBERTEmbedder768",
+    "text/plain": "doclatticeserver.pipeline.embedders.minn_modern_bert_embedder.MinnModernBERTEmbedder768",
+    "text/html": "doclatticeserver.pipeline.embedders.minn_modern_bert_embedder.MinnModernBERTEmbedder768",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "doclatticeserver.pipeline.embedders.minn_modern_bert_embedder.MinnModernBERTEmbedder768",
+}
