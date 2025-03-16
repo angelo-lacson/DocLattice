2344d8b36c172f2aec6a6c2fedf39b8609a91156
angelo-lacson <angelo.lacson.work0407@gmail.com>
2025-03-16 00:05:52 -0500

    Refactored embeddings_tasks to use new manager-provided embeddings creation functions. Switched embeddings generation for doc to a corpus action when doc is added as the embeddings are now based on corpus and not same dimension in all cases.

--- doclatticeserver/documents/apps.py
+++ doclatticeserver/documents/apps.py
@@ -15,6 +15,7 @@ class DocumentsConfig(AppConfig):
             import doclatticeserver.documents.signals  # noqa F401
             from doclatticeserver.documents.models import Document
             from doclatticeserver.documents.signals import (
+                connect_corpus_document_signals,
                 process_doc_on_create_atomic,
             )
 
@@ -23,6 +24,9 @@ class DocumentsConfig(AppConfig):
             post_save.connect(
                 process_doc_on_create_atomic, sender=Document, dispatch_uid=uuid.uuid4()
             )
+
+            # Connect the m2m_changed signal for when documents are added to corpuses
+            connect_corpus_document_signals()
 
         except ImportError:
             pass
