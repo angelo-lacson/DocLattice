4deef7c5f9a0800757c05a74fcc57bcb0c3ceb7c
angelo-lacson <angelo.lacson.work0407@gmail.com>
2025-03-23 18:57:50 -0500

    Fix isse in settings key for doclatticeserver\tests\test_django_annotation_vector_store.py

--- doclatticeserver/tests/test_django_annotation_vector_store.py
+++ doclatticeserver/tests/test_django_annotation_vector_store.py
@@ -216,7 +216,7 @@ class TestDjangoAnnotationVectorStore(TestCase):
         )
 
     @override_settings(
-        DEFAULT_EMBEDDING_PATH="doclatticeserver.pipeline.embedders.sent_transformer_microservice.MicroserviceEmbedder"  # noqa: E501
+        DEFAULT_EMBEDDER="doclatticeserver.pipeline.embedders.sent_transformer_microservice.MicroserviceEmbedder"  # noqa: E501
     )
     def test_query_by_vector_similarity_explicit_embedding(self) -> None:
         """
