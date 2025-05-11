ad72487daa4dc47df53b7d248ad2f98931ecb570
angelo-lacson <angelo.lacson.work0407@gmail.com>
2025-05-11 15:35:57 -0500

    Ran linter. Cleaned up tests. Ensured that sent_transformer_microservice_pulls from component configs.

--- doclatticeserver/pipeline/base/parser.py
+++ doclatticeserver/pipeline/base/parser.py
@@ -16,6 +16,7 @@ from doclatticeserver.utils.importing import (
     import_relationships,
     load_or_create_labels,
 )
+
 from .base_component import PipelineComponentBase
 
 logger = logging.getLogger(__name__)
