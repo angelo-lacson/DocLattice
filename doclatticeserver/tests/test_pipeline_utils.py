db2b3092d2229d50fade53e74ede4db88da002d0
angelo-lacson <angelo.lacson.work0407@gmail.com>
2025-05-18 09:33:19 -0500

    Fixed pipeline test.

--- doclatticeserver/tests/test_pipeline_utils.py
+++ doclatticeserver/tests/test_pipeline_utils.py
@@ -159,6 +159,7 @@ class TestPostProcessor(BasePostProcessor):
         self,
         zip_bytes: bytes,
         export_data: DocLatticeExportDataJsonPythonType,
+        **all_kwargs,
     ) -> Tuple[bytes, DocLatticeExportDataJsonPythonType]:
         # Add logging to debug the process
         logger.info("TestPostProcessor.process_export called")
